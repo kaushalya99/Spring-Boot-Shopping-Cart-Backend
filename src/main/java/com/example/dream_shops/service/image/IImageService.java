8130e8938004411909dfb73b14fea96c145ed0d7
kaushalya99 <hasheweerasooriya@gmail.com>
2024-10-02 21:44:23 +0530

    ImageService file

--- src/main/java/com/example/dream_shops/service/image/IImageService.java
+++ src/main/java/com/example/dream_shops/service/image/IImageService.java
@@ -1,7 +1,13 @@
 package com.example.dream_shops.service.image;
 
 import com.example.dream_shops.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IImageService {
     Image getImageById(Long id);
+    void deleteImageById(Long id);
+    Image saveImage(List<MultipartFile> file, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
 }
